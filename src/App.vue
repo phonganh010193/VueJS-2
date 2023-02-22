16ac1c93e5090c58e1555f5390dfd216aaa7b87d
Phong Anh <phamvanphong010193@gmail.com>
2023-02-22 17:24:09 +0700

    vuejs2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,173 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form class="form-content">
+      <div class="row">
+        <div class="col">
+          <label for="">FirstName</label>
+          <input 
+            type="text" 
+            class="form-control"
+            :value="firstName"
+            @input="(e) => firstName = e.target.value"
+          >
+        </div>
+        <div class="col">
+          <label for="">LastName</label>
+          <input 
+            type="text" 
+            class="form-control" 
+            v-model="lastName"
+          >
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="">Address</label>
+        <input 
+          class="form-control"
+          v-model="address"
+        >
+      </div>
+      <div class="form-group">
+        <label for="">Age</label>
+        <input 
+          class="form-control"
+          v-model="age"
+        >
+      </div>
+      <div class="form-group">
+        <label for="">PhoneNumber</label>
+        <input 
+          class="form-control"
+          v-model="phoneNumber"
+        >
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" v-model="published">
+        <label class="form-check-label" for="">
+          Check me out
+        </label>
+      </div>
+      <button 
+        class="btn btn-primary"
+        @click.prevent="createUser"
+      >Create User</button>
+    </form>
+    <table 
+      :class="[`table table-${theme} table-sm text-center `]"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Adress</th>
+          <th>Age</th>
+          <th>PhoneNumber</th>
+        </tr>
+      </thead>
+      <tbody v-for="(item, index) in listPagesByPublished" v-if="pages.length > 0" :key="index">
+        <tr>
+          <th>{{ fullName(item) }}</th>
+          <td>{{ item.info.address }}</td>
+          <td>{{ item.info.age }} age</td>
+          <td>{{ item.info.phoneNumber }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      <button
+        class="btn btn-primary"
+        @click.prevent="changeThemeTable(table.border)"
+      >TableBorder</button>
+      <button
+        class="btn btn-primary"
+        @click.prevent="changeThemeTable(table.striped)"
+      >TableStriped</button>
+      <button 
+        class="btn btn-primary"
+        @click.prevent="changeThemeTable(table.dark)"
+      >TableDark</button>
+      <button 
+        class="btn btn-primary"
+        @click.prevent="changeThemeTable(table.hover)"
+      >TableHover</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  created() {
+    this.getPages()
+  },
+  data() {
+    return {
+      firstName:'',
+      lastName: '',
+      address: '',
+      age: '',
+      phoneNumber: '',
+      published: true,
+      theme: 'dark',
+      table: {
+        border: 'bordered',
+        dark: 'dark',
+        striped: 'striped',
+        hover: 'hover'
+      },
+      pages:[]
+    }
+  },
+  computed:{ 
+    listPagesByPublished() {
+      return this.pages.filter(el => el.published)
+    }
+  },
+  methods: {
+    fullName(item) {
+      return item.firstName + ' ' + item.lastName;
+    },
+    changeThemeTable(theme) {
+      console.log('theme', theme)
+      this.theme = theme;
+    },
+    async getPages() {
+      let res = await fetch('pages.json')
+      let data = await res.json();
+      this.pages = data
+    },
+    createUser() {
+      const value = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        info: {
+          address:this.address,
+          age:this.age,
+          phoneNumber: this.phoneNumber
+        },
+        published: this.published
+      }
+      console.log(value)
+      this.pages.push(value)
+      this.firstName = '';
+      this.lastName = '';
+      this.address = '';
+      this.age = '';
+      this.phoneNumber = '';
+    }
   }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .form-content {
+    width: 400px;
+    padding-bottom: 10px;
+    margin: 0 auto;
+  }
+
 </style>
+
+
+
